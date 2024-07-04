f7cf6005f6a12d941f204cbb6005d0e815e1ba63
swortiz <ortizsteven61@gmail.com>
2024-07-03 21:07:20 -0700

    finished genius

--- Genius.java
+++ Genius.java
@@ -1,41 +1,48 @@
 public class Genius {
-    public static void Genius(Grid g){
-
-    }
-
-    public int calculate(Grid g){
+    public void calculate(Grid g){
+        int[][] temp = new int[g.getRows()][g.getColumns()];
         for(int i = 0; i < g.getRows(); i++){
             for(int j = 0; j < g.getColumns(); j++){
-                int alive = count(g);
-                if(g.getPoint(i, j) == 1){
+                int alive = count(g, i , j);
+                int current = g.getPoint(i, j);
+                if(current == 1){
                     if(alive < 2 || alive > 3){
-                        g.flipPoint(i, j);
+                        temp[i][j] = 0;
                     }
                     else{
-                        g.flipPoint(i, j);
+                        temp[i][j] = 1;
                     }
                 }
                 else{
                     if(alive == 3){
-                        g.flipPoint(i, j);
+                        temp[i][j] = 1;
+                    }
+                    else{
+                        temp[i][j] = 0;
                     }
                 }
-
-
             }
         }
-        return 0;
+        for(int i = 0; i < g.getRows(); i++){
+            for(int j = 0; j < g.getColumns(); j++){
+                g.setPoint(i, j, temp[i][j]);
+            }
+        }
     }
-    private int count(Grid g){
+    private int count(Grid g, int row, int col){
+        int alive = 0;
         for(int i = -1; i <= 1; i++){
             for(int j = -1; j <= 1; j++){
-
+                if(i == 0 && j == 0) continue;
+                int neighborRow = row + i;
+                int neighborColumns = col + j;
+                if(neighborRow >= 0 && neighborRow < g.getRows() && neighborColumns >= 0 && neighborColumns < g.getColumns()){
+                    alive += g.getPoint(neighborRow, neighborColumns);
+                }
             }
         }
-        return 0;
+        return alive;
     }
-
-
 
 
 }
